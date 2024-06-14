6c79aacc33ec5e7c47daadc6a1005499c730979b
vjchristopher <66867916+vjchristopher@users.noreply.github.com>
2024-06-14 22:25:10 +0530

    Update auction2015.py

--- auction2015.py
+++ auction2015.py
@@ -96,7 +96,7 @@ def app():
         
         dfg.index+=1
         st.dataframe(dfg, column_config={
-        "Frequency-Quantum": st.column_config.Column(
+        "Frequency, Quantum": st.column_config.Column(
             width="large"
         )
         })
