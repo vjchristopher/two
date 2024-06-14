c14fd19e9adc8f906f8ecab10d169b41edd83fba
vjchristopher <66867916+vjchristopher@users.noreply.github.com>
2024-06-14 22:24:39 +0530

    Update auction2013.py

--- auction2013.py
+++ auction2013.py
@@ -106,7 +106,7 @@ def app():
         
         dfg.index+=1
         st.dataframe(dfg, column_config={
-        "Frequency-Quantum": st.column_config.Column(
+        "Frequency, Quantum": st.column_config.Column(
             width="large"
         )
         })
