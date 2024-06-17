df3b8a855c9851cd3987b3f0498141422c7f1f7a
vjchristopher <66867916+vjchristopher@users.noreply.github.com>
2024-06-17 18:12:35 +0530

    Update auction2022.py

--- auction2022.py
+++ auction2022.py
@@ -111,7 +111,14 @@ def app():
             width="large"
         )
         })
-        
+        df_slice=pd.read_csv('Slices_2022_processed.csv')
+        fig = px.treemap(df_slice, path=['Service_Area','Company_Name','Freq_slices'], title='Spectrum slices acquired by the TSPs in 2022 Auction',
+                 )
+        fig.update_layout(
+        autosize=False,
+        width=800,
+        height=1200)
+        st.plotly_chart(fig, theme="streamlit")
 
 
     with revenue:
