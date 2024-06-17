0399dddf511a5ab6962b5046c4c5a25861efb0b9
vjchristopher <66867916+vjchristopher@users.noreply.github.com>
2024-06-17 18:09:59 +0530

    Update auction2016.py

--- auction2016.py
+++ auction2016.py
@@ -107,6 +107,14 @@ def app():
             width="large"
         )
         })
+        df_slice=pd.read_csv('Slices_2016_processed.csv')
+        fig = px.treemap(df_slice, path=['Service_Area','Company_Name','Freq_slices'], title='Spectrum slices acquired by the TSPs in 2016 Auction',
+                 )
+        fig.update_layout(
+        autosize=False,
+        width=800,
+        height=1200)
+        st.plotly_chart(fig, theme="streamlit")
     with revenue:
         #drop duplicates for display.
         
