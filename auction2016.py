2e72f883f6e28d6be091713c87ca1152b620f55e
vjchristopher <66867916+vjchristopher@users.noreply.github.com>
2025-10-30 12:23:27 +0530

    Update auction2016.py

--- auction2016.py
+++ auction2016.py
@@ -169,5 +169,7 @@ def app():
         
 
         #Display the total
-        st.write('',divider='grey')
+        #st.write('',divider='grey')
+        st.divider(color='grey')
         st.subheader(f'3. Consolidated Revenue to Govt from all bidders : :green[Rs.{round(price_grp.Winning_Price.sum(),2)} Crores]')
+
