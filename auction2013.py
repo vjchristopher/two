bce2f836ae624eb2ca52b03f99db0c3b59582b00
vjchristopher <66867916+vjchristopher@users.noreply.github.com>
2026-04-13 15:52:17 +0530

    Update auction2013.py

--- auction2013.py
+++ auction2013.py
@@ -12,11 +12,14 @@ def app():
     with col2:
        st.write("""## Spectrum Auction In Year 2013""")
     
-    df=pd.read_csv('final_results_2013.csv')
-    df['Service_Area']=df['Service_Area'].fillna(method='ffill')
-    df['Band']=df['Band'].fillna(method='ffill')
-    df['Company_Name']=df['Company_Name'].fillna(method='ffill')
-    df['Winning_Price']=df['Winning_Price'].fillna(method='ffill')
+    df=pd.read_csv('final_results_2013.csv')    
+    #df['Service_Area']=df['Service_Area'].fillna(method='ffill')
+    df['Service_Area']=df['Service_Area'].ffill
+    #df['Band']=df['Band'].fillna(method='ffill')
+    df['Band']=df['Band'].ffill
+    #df['Winning_Price']=df['Winning_Price'].fillna(method='ffill')
+    df['Winning_Price']=df['Winning_Price'].ffill
+    
     spectrum_bands=['800 MHz','900 MHz','1800 MHz']
     remarks=['✔️','❌','❌']
     operators=df['Company_Name'].unique().tolist()
